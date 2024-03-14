b4b14a39ed84a4390400b25c818e7fe99f5c6682
hieu.cu_it.weltec <hieu.cu_it.weltec@T607-10>
2024-03-14 13:48:26 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Lab3_Task2;
+
+namespace Lab3_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Vehicle(1234, 4);
+            v1 = new Vehicle("Hieu");
+            v1.printFields();
+            v1.changeSpeed(50.0);
+            v1.stopVehicle();
+
+            Vehicle v2 = new Vehicle(4567, 4);
+            v1 = new Vehicle("Adya");
+            v2.printFields();
+            v1.changeSpeed(100.0);
+            v1.stopVehicle();
+
+            Vehicle v3 = new Vehicle(7891, 4);
+            v1 = new Vehicle("Frances");
+            v2.printFields();
+            v1.changeSpeed(200.0);
+            v1.stopVehicle();
+        }
+
+
+
+    }
+}
+
+
---
+++ Vehicle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_Task2
+{
+    internal class Vehicle
+    {
+        // static fields
+        private static int vehicleID;
+
+        private const int wheels = 4;
+        private static string name;
+        public static int VehicleID { get => vehicleID; set => vehicleID = value; } // can be changed and read
+
+        public static int Wheels => wheels; // should not be changed only read
+
+        public Vehicle(int ID, int wheels)
+        {
+            vehicleID = ID;
+            wheels = 4;
+        }
+        public Vehicle(String ownerName)
+        {
+            name = ownerName;
+        }
+        public void printFields()
+        {
+            Console.WriteLine($"Vehicle's ID is {vehicleID} and its number of wheels is {wheels}");
+            Console.WriteLine($"Vehicle's owner name is {name}");
+        }
+        public double changeSpeed(double speed)
+        {
+            double s = speed;
+            Console.WriteLine($"Vehicle's speed  = {speed}");
+            return s;
+        }
+        public double stopVehicle()
+        {
+            Console.WriteLine($"Vehicle's speed = 0, stopped");
+            return 0;
+        }
+    }
+}
